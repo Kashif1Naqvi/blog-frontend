c8808d68ee7993592ee5d98df0905569512fbab7
Kashif1Naqvi <syedkashifnaqvi14@gmail.com>
2025-10-08 07:26:14 +0500

    minor chnages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://web-production-07a0.up.railway.app/api';
+const API_URL = 'https://web-production-07a0.up.railway.app/api'; //'http://127.0.0.1:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
